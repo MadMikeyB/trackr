3aab29ca0a4e410a189dae923f9f20d4ab550200
Michael Burton <me@mikeylicio.us>
2018-05-27 12:44:46 +0100

    Footer update 🔧

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="footer__container">
-        <p><a href="https://imperfectproduct.co.uk">An Imperfect Product</a></p>
-        <p class="footer__left">Built with &hearts; by @MadMikeyB &copy; 2018</p><!--
+        <p><a href="https://imperfectproduct.co.uk" target="_blank" rel="nofollow noopener">An Imperfect Product</a></p>
+        <p class="footer__left">Built with &hearts; by <a href="https://twitter.com/madmikeyb" target="_blank" rel="nofollow noopener">@MadMikeyB</a> &copy; 2018</p><!--
     --><p class="footer__right"><a href="">Terms</a> &middot; <a href="">Privacy</a></p>
     </div>
 </footer>
