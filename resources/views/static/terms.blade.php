1741b894fc6f1c14164a833be78fdfab849f39a4
Michael Burton <me@mikeylicio.us>
2018-05-28 01:18:50 +0100

    Terms

--- resources/views/static/terms.blade.php
+++ resources/views/static/terms.blade.php
@@ -5,4 +5,67 @@
     <h1>Terms &amp; Conditions</h1>
 </div>
 
+<div class="section">
+    <div class="section__container">
+        <p>mytrackr (The application) is a time tracking application. It is run by one single person and is not a business entity nor is it a registered company.</p>
+
+        <p>Please read these Terms of Use ("Terms", "Terms of Use", "Terms &amp; Conditions") carefully before using the mytrackr application (the Service).</p>
+
+        <p>Your access to and use of the Service is conditional on your acceptance of and compliance with these Terms. You warrant that you are lawfully allowed to enter into binding contracts in your home state, province or country. Children under 13 are not permitted to use the Service.</p>
+
+        <p>By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service.</p>
+
+
+        <p>Our Service allows you to post, link, store, share, upload and otherwise make available certain information, text, graphics, videos, files, or other material ("Content"). You are responsible for the Content that you post to the Service, including its legality, reliability, and appropriateness.</p>
+
+        <p>Any use or reliance on any Content or materials posted via the Services or obtained by you through the Services is at your own risk. We do not endorse, support, represent or guarantee the completeness, truthfulness, accuracy, or reliability of any Content or communications posted via the Services or endorse any opinions expressed via the Services. All Content is the sole responsibility of the person who originated such Content. We may not monitor or control the Content posted via the Services and, we cannot take responsibility for such Content.</p>
+
+        <p>You may not use our service for any unlawful purposes or in furtherance of illegal activities. By using the Service, you agree to comply with all applicable laws governing your online conduct and content. In particular:</p>
+
+        <ul>
+        <li>You must not attempt to harm, disrupt, or otherwise engage in activity that diminishes, the Service's availability, or that of any other computer system or network;</li>
+        <li>You must not attempt to gain access to any account, computers or networks related to the Service without authorization;</li>
+        <li>You must not attempt to obtain any data through any means from the Service, except if we intend to provide or make it available to you;</li>
+        <li>You must not abuse, harass, stalk, threaten, defame or otherwise violate the legal rights (such as rights of privacy and publicity) of others in any form;</li>
+        <li>You must not attempt to interfere with or inhibit another's person's use and enjoyment of the Service;</li>
+        <li>You must not harvest or otherwise collect information about others, including email addresses;</li>
+        <li>You must not create a false identity, multiple identities or impersonate another; and</li>
+        <li>You must respect the personal data of our users.</li>
+        </ul>
+
+        <p>You may need to create an account to use some of our Services. When you create an account with us, you must provide us information that is accurate, complete, and current at all times. Failure to do so constitutes a breach of the Terms, which may result in immediate termination of your account on our Service.</p>
+
+        <p>You are responsible for safeguarding the password that you use to access the Service and agree not to disclose your password to any third party. You agree to be fully responsible for activities that relate to your account or your password. You must notify us immediately upon becoming aware of any breach of security or unauthorized use of your account.</p>
+
+        <p>You may not use as a username the name of another person or entity or that is not lawfully available for use, a name or trademark that is subject to any rights of another person or entity other than you without appropriate authorization, or a name that is otherwise offensive, vulgar or obscene.</p>
+
+        <p>We may terminate or suspend your account immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach the Terms.</p>
+
+        <p>Upon termination, your right to use the Service will immediately cease. If you wish to terminate your account, you may simply discontinue using the Service.</p>
+
+        <p>All provisions of the Terms which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.</p>
+
+        <p>We shall not be liable to you or any third party for any claims or damages arising out of any termination or suspension or any other actions taken by us in connection therewith.</p>
+
+        <p>If applicable law requires us to provide notice of termination or cancellation, we may give prior or subsequent notice by posting it on the Service or by sending a communication to any address (email or otherwise) that we have for you in our records.</p>
+
+        <p>Your use of the Service is at your sole risk. The Service is provided on an "AS IS" and "AS AVAILABLE" basis. The Service is provided without warranties of any kind, whether express or implied, including, but not limited to, implied warranties of merchantability, fitness for a particular purpose, non-infringement or course of performance.</p>
+
+        <p>mytrackr does not warrant that a) the Service will function uninterrupted, secure or available at any particular time or location; b) any errors or defects will be corrected; c) the Service is free of viruses or other harmful components; or d) the results of using the Service will meet your requirements.</p>
+
+        <p>This disclaimer of liability applies to any damages or injury caused by any failure of performance, error, omission, interruption, deletion, defect, delay in operation or transmission, computer virus, communication line failure, theft, or destruction or unauthorized access or, alteration of or use of record in connection with the use or operation of the Service, whether for breach of contract, tortious behaviour, negligence or any other cause of action.</p>
+
+        <p>We make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability or availability with respect to the content contained on the Service for any purpose. Any reliance you place on such information is therefore strictly at your own risk. We disclaim any express or implied warranty representation or guarantee as to the effectiveness or profitability of the Service or that the operation of our Service will be uninterrupted or error-free. We are not liable for the consequences of any interruptions or error in the Service.</p>
+
+        <p>These Terms shall be governed and construed in accordance with the laws of the United Kingdom, without regard to its conflict of law provisions. Any disputes will be heard in the courts of that state under English law.</p>
+
+        <p>Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights. If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these Terms will remain in effect. These Terms constitute the entire agreement between us regarding our Service, and supersede and replace any prior agreements we might have between us regarding the Service.</p>
+        
+
+        <p>We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will try to provide at least 30 days notice prior to any new terms taking effect.</p>
+
+        <p>It is your sole responsibility to periodically check these Terms for any changes. If you do not agree with any of the changes to these Terms, it is your sole responsibility to stop using the Service. Your continued use of the Service will be deemed as your acceptance thereof.</p>
+    </div>
+</div>
+
 @endsection
