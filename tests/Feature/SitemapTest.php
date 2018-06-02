0f96aa6df43cfcb6408e96beaad219d032e51290
Michael Burton <me@mikeylicio.us>
2018-06-02 20:42:12 +0100

    ✅ Sitemap test

---
+++ tests/Feature/SitemapTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SitemapTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_the_application_can_have_a_sitemap_index()
+    {
+        $this->get(route('sitemap.index'))->assertStatus(200);    
+    }
+
+    public function test_the_application_can_have_a_sitemap_for_pages()
+    {
+        $this->get(route('sitemap.pages'))->assertStatus(200);       
+    }
+}
